f7ff621cd3f2edaba255f6c8244218f103299d42
Alexander Paul <exactfalse@gmail.com>
2020-03-18 15:10:49 +0800

    prettified and deleted unused static files

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -3,11 +3,5 @@ import React from 'react';
 import './search-box.styles.css';
 
 export const SearchBox = ({ placeholder, handleChange }) => (
-    <input 
-        className='search'
-        type='text' 
-        placeholder={ placeholder }
-        onChange={ handleChange } 
-    />
-
+	<input className='search' type='text' placeholder={placeholder} onChange={handleChange} />
 );
